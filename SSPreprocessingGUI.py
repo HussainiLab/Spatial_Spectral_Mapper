d85b10ef4eea7d67d8859a29dc1cd63a576dc3da
GeoffBarrett <Geoffrey.M.Barrett@gmail.com>
2018-01-22 22:40:29 -0500

    updated the default chunk and overlap size to 5s and 4s respectively, should provide quicker result

--- SSPreprocessingGUI.py
+++ SSPreprocessingGUI.py
@@ -258,10 +258,10 @@ class Window(QtGui.QWidget):  # defines the window class (main window)
         self.analyzed_files = []
 
         if self.chunk_size.text() == '':
-            self.chunk_size.setText('30')
+            self.chunk_size.setText('5')
 
         if self.chunk_overlap.text() == '':
-            self.chunk_overlap.setText('1.2')
+            self.chunk_overlap.setText('4')
 
         self.recording_queue.clear()
         self.eeg_types.clear()
