1b1ab651589f54f962105a85369a3f785477a903
Hussaini Lab <34137235+HussainiLab@users.noreply.github.com>
2021-05-24 13:45:37 -0400

    Added test file egg_testing.py

---
+++ eeg_testing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 15:47:42 2021
+
+@author: vajra
+"""
+
+from core.Tint_Matlab import get_active_eeg, remEEGShift, ReadEEG
+from core.ProcessingFunctions import is_processed
+
+# set_file = r'C:\Users\vajra\Documents\data\20140815-behavior2-90\20140815-behavior2-90.set'
+# eeg_file = r'C:\Users\vajra\Documents\data\20140815-behavior2-90\20140815-behavior2-90.eeg'
+
+# active_EEG = get_active_eeg(set_file)
+# eeg = ReadEEG(eeg_file)
+# print(is_processed(set_file))
